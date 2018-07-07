90cbc3ed87ac66da234e8fb16625ba3a1a68b2e7
Digo498 <rodrigotp98@gmail.com>
2018-07-07 01:37:57 -0300

    Fixed density of states script

--- scripts/densityofstates.py
+++ scripts/densityofstates.py
@@ -2,24 +2,29 @@ import matplotlib.pyplot as plt
 from masses import *
 import numpy as np
 
-size=0.1
-m = 40
+size= 0.4
+m = int(4/size)
 
-x_axis = np.arange(0, 4, size)
+w2 = np.arange(0, 4, size)
 counts = np.zeros(shape=(m))
+x_axis = np.zeros(shape=(m))
 
-for i in range(0,40):
+for i in range(0,m):
     for j in range(0,len(eigvalues)):
-        if eigvalues[j] < x_axis[i]+0.1 and eigvalues[j] >= x_axis[i]:
+        if eigvalues[j] < w2[i]+size and eigvalues[j] >= w2[i]:
             counts[i]+=1
+
+for i in range(0,len(w2)):
+    x_axis[i] = w2[i]+size/2
+
 
 print(counts)
 plt.title('Densidade de estados')
-plt.bar(x_axis, counts, width=0.1, color='w', edgecolor='r')
+plt.bar(x_axis, counts, width=size, color='w', edgecolor='r')
 plt.xticks(np.arange(0, 4.1, 0.4))
-plt.yticks(np.arange(0,16,1))
+plt.yticks(np.arange(0,100,1))
 plt.xlabel(r'$\omega^2$')
-plt.axis([-0.05, 4, 0, 12])
+plt.axis([-0.05, 4, 0, 30])
 #plt.xticks(x_axis)
 plt.ylabel('Count')
 
